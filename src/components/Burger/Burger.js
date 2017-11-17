a01522ef6b2186c81e5f7504f69e2da91ac85df1
Shenloc <pbroniarczyk@outlook.com>
2017-11-17 18:20:15 +0100

    dynamic burger - without user's input

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,11 +7,16 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const Burger = ( props ) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(e => {
+            return [...Array(props.ingredients[e])].map((_, i) => {
+                return <BurgerIngredient key={e + i} type={e} />;
+            });
+        });
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     )
